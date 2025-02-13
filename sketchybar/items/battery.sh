7d908f13f0c5b895dcf04f819d91c2dc2531329b
Adam D'Agostino <adamdagostino@mac.mynetworksettings.com>
2025-02-12 22:44:31 -0500

    Made battery item dynamic so it only shows on laptops.

--- sketchybar/items/battery.sh
+++ sketchybar/items/battery.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sketchybar --add item battery right \
-           --set battery update_freq=120 \
-                         script="$PLUGIN_DIR/battery.sh" \
-           --subscribe battery system_woke power_source_change
+# Check if the machine has a battery
+if [[ $(system_profiler SPPowerDataType | grep "Battery Information") ]]; then
+  sketchybar --add item battery right \
+             --set battery update_freq=120 \
+                           script="$PLUGIN_DIR/battery.sh" \
+             --subscribe battery system_woke power_source_change
+else
+  # No battery, do nothing
+  echo "No battery detected. Skipping battery item."
+fi
