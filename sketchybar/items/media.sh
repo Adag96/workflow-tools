53cd44aff70016738bc77c15f8f979bced6fe415
Adam D'Agostino <adamdagostino@mac.mynetworksettings.com>
2025-02-15 14:12:18 -0500

    Made media player item have longer text for Mac Studio

--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Get the machine name
+MACHINE_NAME=$(scutil --get ComputerName)
+
+# Set device-specific configurations
+if [[ "$MACHINE_NAME" == *"Studio"* ]]; then
+    MAX_CHARS=40  # Larger value for Mac Studio
+else
+    MAX_CHARS=24  # Default to MacBook Pro value
+fi
+
 sketchybar --add item media right \
           --set media label.color=$ACCENT_COLOR \
-                      label.max_chars=24 \
+                      label.max_chars=$MAX_CHARS \
                       icon.padding_left=0 \
                       scroll_texts=on \
                       icon=􀑪 \
