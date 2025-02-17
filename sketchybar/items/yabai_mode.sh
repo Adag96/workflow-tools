4ef9299f18bce13bd50657f1d2977909dada14df
Adag96 <adag96@gmail.com>
2025-02-17 18:29:18 -0500

    Improved yabai mode feature

--- sketchybar/items/yabai_mode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "$CONFIG_DIR/colors.sh"
-
-sketchybar --add item yabai_mode right \
-           --set yabai_mode background.color=$ACCENT_COLOR \
-                           label.color=$BAR_COLOR \
-                           background.drawing=on \
-                           script="$PLUGIN_DIR/yabai_mode.sh"
