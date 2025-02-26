3a412311e94b2e6f5b73c6e3bea218608171c4fd
Adam D'Agostino <adamdagostino@Adams-Mac-Studio.local>
2025-02-26 17:49:12 -0500

    Moved Ableton timer to front app space

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -8,5 +8,6 @@ sketchybar --add item front_app left \
                                 icon.font="sketchybar-app-font:Regular:15.0" \
                                 label.font="$TEXT_FONT:Bold:15.0"\
                                 label.color=$ACTIVE_SPACE_TEXT_COLOR \
+                                update_freq=1 \
                                 script="$PLUGIN_DIR/front_app.sh" \
            --subscribe front_app front_app_switched
