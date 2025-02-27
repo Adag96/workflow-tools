da0d2c865300eee55e2b62dd65a74a552598060c
Adag96 <adag96@gmail.com>
2025-02-27 15:35:05 -0500

    Timer now clears 'Untitled' project data on Live re-launch. 'Untitled' time data now transfers to newly saved projects.

--- sketchybar/items/battery.sh
+++ sketchybar/items/battery.sh
@@ -5,6 +5,7 @@ if [[ $(system_profiler SPPowerDataType | grep "Battery Information") ]]; then
   sketchybar --add item battery right \
              --set battery update_freq=120 \
                            script="$PLUGIN_DIR/battery.sh" \
+                           NAME=battery \
              --subscribe battery system_woke power_source_change
 else
   # No battery, do nothing
